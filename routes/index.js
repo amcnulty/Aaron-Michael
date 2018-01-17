97b32bba43c598c490ab7405b20282b31b1f8217
amcnulty <amcnulty88@swbell.net>
2018-01-16 20:58:44 -0800

    Project Posters
    Created the project poster section that displays the different projects.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const projects = require('../lib/projects');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aaron Michael | A Personal Website For Aaron Michael McNulty' });
+  res.render('index', { 
+    title: 'Aaron Michael | A Personal Website For Aaron Michael McNulty',
+    projects: projects
+  });
 });
 
 module.exports = router;
