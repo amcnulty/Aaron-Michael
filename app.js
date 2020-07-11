0eaabcec0f165cf3032d5424042e5da7502e3e9f
amcnulty <amcnulty88@swbell.net>
2020-07-11 14:24:05 -0500

    Added cors to nucamp-data

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var nucampData = require('./routes/nucampData');
@@ -32,7 +33,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/nucamp-data', nucampData);
+app.use('/nucamp-data', cors(), nucampData);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
